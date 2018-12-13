958d5e0c28796aa0c9c8151955c81d606ca01e51
liuchun <liuchun@logread.com>
2018-12-13 10:47:42 +0800

    调整测试程序

--- main.c
+++ main.c
@@ -8,30 +8,25 @@ static int do_tree(struct avl_inode *tree)
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int status;
+	int i = 0;
+	int status = 0;
 	struct avl_inode *root = NULL;
 	struct avl_inode *inode = NULL;
-	int a[]={3,2,1,4,5,6,7,10,9,8,14,12,13,11,15,15,16,15,20,21,22,19,18,17};
+	int key[] = {3,2,1,4,5,6,7,10,9,8,14,12,13,11,15,15,16,15,20,21,22,19,18,17};
 
-	for(i = 0;i < sizeof(a)/sizeof(int); i++) {
-		status = 0;
-		inode = avl_alloc(a[i]);
-
-		//printf("root inode:%p key:%d insert %d->%p", 
-		//		root, root?root->key:-1, a[i], inode);
-		root = avl_insert(root, inode, &status);
-		//printf(" status:%08X\n", status);
+	for(i = 0;i < sizeof(key)/sizeof(int); i++) {
+		root = avl_insert(root, avl_alloc(key[i]), &status);
 	}
+	
 	printf("中序遍历二叉平衡树:\n");
 	avl_doit(root, do_tree);
 	printf("\n");
 
-	printf("删除结点元素5后中序遍历:\n");
 	inode = avl_search(root, 5);
 	root = avl_delete(root, inode);
 	avl_free(inode);
 
+	printf("删除结点元素5后中序遍历:\n");
 	avl_doit(root, do_tree);
 	printf("\n");
 
